a1aafaa2e899fbc87d7e8c266d373ce04656cf55
Svetlana <kutygina.sveta@mail.ru>
2022-10-23 01:23:40 +0300

    Задача 7 (последняя цыфра трехзначного числа)

---
+++ Tesk07/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая 
+// принимает на вход трёхзначное число 
+// на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+ if(number > 99 && number < 1000)
+ {
+int lastDigit = number % 10; // 456 /10 = 45,6
+Console.WriteLine($"Последняя цифра числа = {lastDigit}");
+ }
+ else Console.WriteLine("Введите трехзначное число");
