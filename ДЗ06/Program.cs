5f8e90d3f19ea1cdfeef0a7820d4e5411dc45cb4
Svetlana <kutygina.sveta@mail.ru>
2022-10-24 22:28:24 +0300

    Задача №6 деление на 2 без остатка

---
+++ ДЗ06/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая 
+// на вход принимает число
+// выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    int lastDigit = number % 2; // 457 /2 = 228.1
+    Console.WriteLine("Да");
+}
+else Console.WriteLine("Нет");
