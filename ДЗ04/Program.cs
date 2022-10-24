f7faee67ae5ae401b970b5a7c8ed139656a826e6
Svetlana <kutygina.sveta@mail.ru>
2022-10-24 18:35:58 +0300

    Задача №4 наибольшее из трех чисел

---
+++ ДЗ04/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая 
+// принимает на вход три числа
+// выдаёт максимальное из этих чисел
+// 2, 3, 7 -> 7
+// 44, 5, 78 -> 78
+// 22, 3, 9 -> 22
+
+        Console.Write("Введите первое целое число: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите второе целое число: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите третье целое число: ");
+        int c = Convert.ToInt32(Console.ReadLine());
+        int max = 0;
+        if (a > max && a > b && a > c)
+        {
+            Console.Write($" максимальное число = {a}");
+        }
+         else if (b > max && b > a && b > c)
+        {
+            Console.Write($" максимальное число = {b}");
+        }
+        else if (c > max && c > a && c > b)
+        {
+            Console.Write($" максимальное число = {c}");
+        }
+else
+        {
+            Console.WriteLine($"max = {max}");
+        }
